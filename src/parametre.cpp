3ecc7bcb9abf0d29576dca811bff145b01e8f259
Quentin Robert <robertquentin29@gmail.com>
2022-11-10 10:34:30 +0100

    Ajout des variables nbTentatives et tentatives + initialisation

--- src/parametre.cpp
+++ src/parametre.cpp
@@ -16,20 +16,25 @@ int main()
     //VARIABLE
     const unsigned short int NB_IMAGE_BASE = 9;
     const unsigned short int NB_POLICE_BASE = 9;
-    short unsigned int nbCarac;     // nombre de caractère de la chaine choisi par l'admin
-    short unsigned int nbPolice;    // nombre de police à appliquer sur la chaine
-    short unsigned int nbEffet;     // nombre d'effet à appliquer sur la chaine
-    short unsigned int difficulte;  // le niveau de difficulté des effets que l'ont va appliquer
-    short unsigned int policeCarac; // police a appliquer sur une image
-    short unsigned int *chaineOrig; // la chaine d'origine qui sera altérer par les effets
-    short unsigned int nbImage[NB_POLICE_BASE];    // tableau qui contient le nombre d'image dansz chaque police de caractère que l'on a 
-    short unsigned int basePolice[NB_POLICE_BASE];   // tableau contenant toute les polices que l'on va utiliser
+    unsigned short int nbCarac;     // nombre de caractère de la chaine choisi par l'admin
+    unsigned short int nbPolice;    // nombre de police à appliquer sur la chaine
+    unsigned short int nbEffet;     // nombre d'effet à appliquer sur la chaine
+    unsigned short int difficulte;  // le niveau de difficulté des effets que l'ont va appliquer
+    unsigned short int policeCarac; // police a appliquer sur une image
+    unsigned short int *chaineOrig; // la chaine d'origine qui sera altérer par les effets
+    unsigned short int nbImage[NB_POLICE_BASE];    // tableau qui contient le nombre d'image dansz chaque police de caractère que l'on a 
+    unsigned short int basePolice[NB_POLICE_BASE];   // tableau contenant toute les polices que l'on va utiliser
+    unsigned short int nbTentative;
+    unsigned short int tentative = 0;
     
-    for (int i = 0; i < NB_POLICE_BASE; i++)
+    for (unsigned short int i = 0; i < NB_POLICE_BASE; i++)
     {
         nbImage[i] = NB_IMAGE_BASE;
     }
-    
+
+    // Demander combien de tentative l'utilisateur a droit
+    cout << "Combien de tentative voulez-vous pour réussir le captcha ? " << endl;
+    cin >> nbTentative;
 
     //parametrer la génération de la chaîne 
     
@@ -52,7 +57,7 @@ int main()
     //générer la chaine de caractere
     chaineOrig= new short unsigned int[nbCarac];
     //nbImage= new short unsigned int[sizeof basePolice];
-    for (int i = 0; i < nbCarac; i++)
+    for (unsigned short int i = 0; i < nbCarac; i++)
     {
         //choix de la police aléatoirement
         policeCarac = rand() % nbPolice;
