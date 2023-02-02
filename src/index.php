32a68cce08b7e74a5dbd9ab4dcd3b51ef54f60d8
Tanguy Lauriou <tlauriou@gmail.com>
2023-02-02 11:23:13 +0100

    affichage trié des billet et possibilité de recupération de l'id par clic dessus

--- src/index.php
+++ src/index.php
@@ -1,15 +1,26 @@
 <?php 
-    include('ConnBD.php');
-    $conn = ConnBD();
-    session_start();
-    $query = "SELECT * FROM Billet;";
-    $result = mysqli_query($conn, $query);
+// Connexion à la base de données
+include('ConnBD.php');
+$conn = ConnBD();
+session_start();
 
-    
-    $billets = mysqli_fetch_array($result, MYSQLI_BOTH);
-    //print_r($billets);
+// Requête pour récupérer tous les billets
+$query = "SELECT * FROM Billet;";
+$result = mysqli_query($conn, $query);
+
+// Stockage des billets dans un tableau PHP
+$billets = array();
+while ($row = mysqli_fetch_assoc($result)) {
+  $billets[] = $row;
+}
+
+// Initialisation des tableaux pour chaque genre
+$sports = array();
+$concerts = array();
+$festivals = array();
+
+// Parcours des billets et classement selon leur genre
 ?>
-
 <!DOCTYPE html>
 <HTML>
     <head>
@@ -18,61 +29,86 @@
         <script src="https://kit.fontawesome.com/7c2235d2ba.js" crossorigin="anonymous"></script>
         <title>Tickets'Press</title>
     </head>
-
-
-
     <body>
+        <!-- Entête de la page -->
         <header>
-            <button>Vendre ses billets</button>
-        
-            <input id="searchbar" onkeyup="search_ticket()" type="text"
-            name="search" placeholder="Search tickets..">
-            <i class="fa-solid fa-user"></i>
         </header>
-        
-        <main>
-            <bw-promotion>
-                <div class="categories">
-                    <div role="tab" class="active" id="#nouveautes">Nouveautés</div>
-                    <div role="tab" id="#sport">Sport</div>
-                    <div role="tab" id="#festival">Festival</div>
-                    <div role="tab" id="#concert">Concert</div>
-                </div>
-                <?php 
-              
-                foreach ($billets as $billet){
-                    echo "<div class='billet'>";
-                    echo "<h2> $billets[0] </h2> ";
-                    echo "<p> $billet[7] </p>";
-        
-                    
-                    /*if ($billet[4] == " sport "){
-                    echo "<div class='category sport'>Sport</div>";}
-                    elseif ($billet[4] == "festival"){ 
-                        echo "<div class='category festival'>Festival</div>";}
-                    elseif ($billet[4] == "concert"){
-                        echo "<div class='category concert'>Concert</div>";}
-                    else {
-                    echo "<div class='category nouveautes'>Nouveautés</div>";}*/
-                    //echo "</div>";
-                    }
-                ?>
-                
 
-                <div role="tab" class="mat-tab-label mat-tab-label-active" id="mat-tab-label-1-0" tabindex="0" aria-posinset="1" aria-setsize="2" aria-controls="mat-tab-content-1-0" aria-selected="true" aria-disabled="false"></div>
-            </bw-promotion>
-            
+        <!-- Contenu principal de la page -->
+        <main>
+            <!-- Formulaire pour sélectionner la date -->
             <form>
                 <label for="DATE">Date: </label>
+                <!-- Champ pour sélectionner la date -->
                 <div><input type="date" id="DATE" name="DATE"
                     value="<?php echo $thedate=date('Y-m-d'); ?>"
                     min="<?php echo date('Y-m-d');?>" max="<?php echo date('Y-m-d', strtotime('+5 year'));?>"></div>
             </form>
-            
         </main>
 
+        <!-- Pied de page -->
         <footer>
-            
         </footer>
     </body>
 </HTML>
+
+
+
+<?php
+foreach ($billets as $billet) {
+  $genre = trim($billet['genre']);
+  switch ($genre) {
+    case 'sport':
+      $sports[] = $billet;
+      break;
+    case 'concert':
+      $concerts[] = $billet;
+      break;
+    case 'festival':
+      $festivals[] = $billet;
+      break;
+  }
+}
+
+
+// Affichage des billets pour chaque genre
+echo '<h2>Sports</h2>';
+echo '<div style="display: flex;">';
+foreach ($sports as $sport) {
+  echo '<div style="width: 175px; word-wrap: break-word;">';
+  echo '<a href="achat.php?id=' . $sport['id'] . '">';
+  echo '<img src="images/sport.jpg"> <br>';
+  echo '<span style="font-size: smaller;">' . $sport['libelle'] . '</span><br>';
+  echo '</a>';
+  echo '</div>';
+}
+echo '</div>';
+
+echo '<h2>Concerts</h2>';
+echo '<div style="display: flex;">';
+foreach ($concerts as $concert) {
+  echo '<div style="width: 175px; word-wrap: break-word;">';
+  echo '<a href="achat.php?id=' . $concert['id'] . '">';
+  echo '<img src="images/concert.jpg"> <br>';
+  echo '<span style="font-size: smaller;">' . $concert['libelle'] . '</span><br>';
+  echo '</a>';
+  echo '</div>';
+}
+echo '</div>';
+
+echo '<h2>Festivals</h2>';
+echo '<div style="display: flex;">';
+foreach ($festivals as $festival) {
+  echo '<div style="width: 175px; word-wrap: break-word;">';
+  echo '<a href="achat.php?id=' . $festival['id'] . '">';
+  echo '<img src="images/festival.jpg"> <br>';
+  echo '<span style="font-size: smaller;">' . $festival['libelle'] . '</span><br>';
+  echo '</a>';
+  echo '</div>';
+}
+echo '</div>';
+
+
+?>
+
+
