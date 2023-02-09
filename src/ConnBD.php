2182c903ba08973cc117f4c5a5923dee90e11674
Matéo Alves <backwolf4728@gmail.com>
2023-02-09 09:14:24 +0100

    Amélioration du CSS et modification de ConnBD en utilisant le PDO

--- src/ConnBD.php
+++ src/ConnBD.php
@@ -5,7 +5,6 @@
     * @author Quentin ROBERT, Matéo ALVES, Tanguy LAURIOU, Juan David RODRIGUEZ SINCLAIR
     * @brief Prodédure qui permet de se connecter à la base de données
     * @date 2023-01-16
-    * 
     */
 
 
@@ -24,12 +23,21 @@
         $password = 'tlauriou_bd';
         $bdd = 'tlauriou_bd';
         
-        // On établit la connexion
-        $conn = mysqli_connect($servername,$username,$password,$bdd);
+        // On établit la connexion en PDO pour sécurer la connexion
+        $conn = new PDO("mysql:host=$servername;dbname=$bdd", $username, $password);
 
+        // Cherche l'id de l'utilisateur dans la base de donnée
         $query  = "SELECT idUti FROM Utilisateur WHERE mail = $mail;";
+
         // Définir une variable pour enregistrer le user_id utilisateur
-        $_SESSION['user_id'] = mysqli_query($conn, $query);
+        $_SESSION['user_id'] = $conn->query($query);
+        
         return $conn;
+
+        //mysqli_query
+        //$conn = mysqli_connect($servername,$username,$password,$bdd);
+        // $query  = "SELECT idUti FROM Utilisateur WHERE mail = $mail;";
+        //$_SESSION['user_id'] = mysqli_query($conn, $query);
+
     }
 ?>
