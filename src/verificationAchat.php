1ac5f7d54cdfeb8b739416b9113acb8185fe89ab
Matéo Alves <backwolf4728@gmail.com>
2023-03-13 10:15:21 +0100

    Ajout d'un billet à la table ventre une fois qu'il est vendu

--- src/verificationAchat.php
+++ src/verificationAchat.php
@@ -9,51 +9,54 @@ $dateExpiration = $_POST['dateExpiration'];
 
 if (isset($_POST['codeCaptcha']) && $_POST['codeCaptcha'] == $_SESSION['captcha']) {
     if ($dateExpiration > date("Y-m-d")) {
-        header('Location: index.php');
+        //header('Location: index.php');*/
+
+        // Date du jour
+        $date = date("Y-m-d");
         foreach ($_SESSION['panier'] as $article) {
+
             $id = $article['id'];
             // prépare la requête qui retire un à la quantité de l'article dans la base de données en PDO
             $stmt = $conn->prepare("UPDATE Billet SET quantite = quantite - 1 WHERE id = :id");
             $stmt->bindParam(':id', $id);
             // On exécute la requête
             $stmt->execute();
+            $stmt->closeCursor();
 
-            
-            $stmt = $conn->prepare("DELETE FROM Billet WHERE id = :id AND quantite = 0");
+            // supprime le billet en question de la Base de donnée si la quantitité est égale à 0
+            /*$stmt = $conn->prepare("DELETE FROM Billet WHERE id = :id AND quantite = 0");
             $stmt->bindParam(':id', $id);
             $stmt->execute();
+            $stmt->closeCursor();*/
 
-            // Insère le billet dans la table vente
-            //$date = date("Y-m-d");
-
-            // récupérer le nombre de billet vendu
-            //$query = $conn->query("SELECT COUNT(*) FROM Vente");
             // prépare la requête qui affiche le nombre de billet vendu dans la base de données en PDO
-            /*$query = $conn->prepare("SELECT COUNT(*) FROM Vente");
-            $query->execute();
-
+            $stmt = $conn->prepare("SELECT COUNT(*) FROM Vente");
+            $stmt->execute();
             // converti le contenu de $query en number
+            $idVente = ($stmt->fetchColumn()) + 1;
+            $stmt->closeCursor();
             
-            $idVente = ($query->fetchColumn()) + 1;
-            var_dump($idVente);
-
+            // récupère le prix du billet en question
+            $stmt = $conn->prepare("SELECT prix FROM Billet WHERE id = :id");
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            $prix = $stmt->fetchColumn();
+            $stmt->closeCursor();
+            
             //insertion dans la table Vente
             $stmt = $conn->prepare("INSERT INTO Vente (idVente, dateVente, prix, idBillet, idAcheteur) VALUES (:idVente, :dateVente, :prix, :idBillet, :idAcheteur)");
             $stmt->bindParam(':idVente', $idVente);
             $stmt->bindParam(':dateVente', $date);
-            $stmt->bindParam(':prix', $article['prix']);
+            $stmt->bindParam(':prix', $prix);
             $stmt->bindParam(':idBillet', $id);
             $stmt->bindParam(':idAcheteur', $_SESSION['user_id']);
-            $stmt->execute();*/
-
-
-            // supprime le billet en question de la Base de donnée si la quantitité est égale à 0
-
+            $stmt->execute();
+            $stmt->closeCursor();
             
         }
         // On supprime le panier
         unset($_SESSION['panier']);
-        //header('Location: index.php');
+        header('Location: index.php');
         
 
     } else {
